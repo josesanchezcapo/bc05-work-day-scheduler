e5654eac605f457ed680d4cb40f81d833431cefa
Jose Sanchez-Capo <josesanchezcapo@me.com>
2020-11-04 10:39:28 -0600

    set timeblocks state

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,7 +1,34 @@
 
+// Define object time
+var timeblock = ['8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18']
+
+
 // Define today using moment()
 var today = moment().format('MMMM Do YYYY, h:mm:ss a');
 // console.log today's date
 console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
 // Render today's date to the page
 $('#displayDate').append(today);
+
+// Set time blocks state
+
+var currentTime = moment().format('h:mm a');
+
+for (var i = 0; i < timeblock.length; i++) {
+
+    var currentHour = $('#' + timeblock[i]).data('value');
+    var nextHour = $('#' + timeblock[i + 1]).data('value');
+
+    if (currentTime > currentHour) {
+
+        $('#' + timeblock[i]).removeClass('col').addClass('col past');
+    }
+    if (currentTime < currentHour) {
+        $('#' + timeblock[i]).removeClass('col').addClass('col future');
+    }
+    if (currentTime === currentHour || currentTime < nextHour) {
+        $('#' + timeblock[i]).removeClass('col').addClass('col present');
+    }
+
+}
+
